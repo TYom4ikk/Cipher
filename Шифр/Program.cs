0579b10f399f5ce2573675546d01eb8c54b009d4
TYom4ikk <Zhakubekov20032006@gmail.com>
2023-11-26 23:47:04 +0500

    Final Developing of Cipher logic

--- Шифр/Program.cs
+++ Шифр/Program.cs
@@ -14,15 +14,10 @@ namespace Шифр
             После шифрования и расшифровки сообщения программа должна предложить
             пользователю продолжить или завершить работу.
         */
+
         static void Main(string[] args)
         {
-            /* string myString = "Пример строки";
-             int hashCode = myString.GetHashCode();
-             Console.WriteLine(hashCode);
-             Console.ReadKey();*/
-
-
-            int shift = 7; // сдвиг
+            int shift = 10; // сдвиг
 
             int choice;
             while (true)
@@ -64,59 +59,32 @@ namespace Шифр
                 {
                     Console.WriteLine(ex.Message);
                 }
-            }     /*try
-                    {
-                        
-                        using (RSA rsa = RSA.Create())
-                        {
-                           
-                            RSAParameters publicKey = rsa.ExportParameters(false);
-                            RSAParameters privateKey = rsa.ExportParameters(true);
-
-                        
-                            string originalText = "Hello, world!";
-
-                            
-                            byte[] encryptedData = Encrypt(Encoding.UTF8.GetBytes(originalText), publicKey);
-                            byte[] decryptedData = Decrypt(encryptedData, privateKey);
-
-                            string decryptedText = Encoding.UTF8.GetString(decryptedData);
-                            Console.WriteLine("Исходный текст: " + originalText);
-                            Console.WriteLine("Зашифрованные данные: " + Convert.ToBase64String(encryptedData));
-                            Console.WriteLine("Расшифрованный текст: " + decryptedText);
-                        }
-                    }
-                    catch (CryptographicException e)
-                    {
-                 
-                }*/
+            }     
         }
         static string Encrypt(string str, int shift)
         {
-            char[] str_chars = str.ToCharArray();
             string encrypted_str_chars = string.Empty;
             int code;
-            for (int i = 0; i < str_chars.Length; i++)
+            for (int i = 0; i < str.Length; i++)
             {
-                code = int.Parse(str_chars[i].ToString());
-                code += code + shift;
-                encrypted_str_chars += code.ToString();
+                code = ((int)str[i]) + shift;
+                if (code > 255) { code -= 256; }
+                encrypted_str_chars += (char)code;
             }
             return encrypted_str_chars;
         }
 
         static string Decrypt(string str, int shift)
         {
-            char[] str_chars = str.ToCharArray();
-            string encrypted_str_chars = string.Empty;
+            string decrypted_str_chars = string.Empty;
             int code;
-
-            for (int i = 0; i < str_chars.Length; i++)
+            for (int i = 0; i < str.Length; i++)
             {
-                code = ((int.Parse(str_chars[i].ToString())) - shift);
-                encrypted_str_chars += code.ToString();
+                code = ((int)str[i]) - shift;
+                if (code < 0) { code += 256; }
+                decrypted_str_chars += (char)code;
             }
-            return encrypted_str_chars;
+            return decrypted_str_chars;
         }
 
         static bool Again()
